3dd8937f75419fe537531f5a1fc52ed84cba3be6
GermanoGiudici <germano.giudici@gmail.com>
2022-02-11 18:23:57 +0100

    fix on kc

--- patch-kc.sh
+++ patch-kc.sh
@@ -2,5 +2,5 @@
 user=$(sudo kubectl -n entando get secrets/quickstart-kc-admin-secret --template={{.data.username}} | base64 -d)
 pass=$(sudo kubectl -n entando get secrets/quickstart-kc-admin-secret --template={{.data.password}} | base64 -d)
 dns=$(echo $HOSTNAME.$INSTRUQT_PARTICIPANT_ID.instruqt.io)
-./kc-adapter -u  https://\$dns \$user \$pass
+./kc-adapter -u  https://$dns $user $pass
 
