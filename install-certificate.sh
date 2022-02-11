f420f911f18809ccfec04919e55dcbc627d2e3d1
GermanoGiudici <germano.giudici@gmail.com>
2022-02-11 18:04:06 +0100

    fix

--- install-certificate.sh
+++ install-certificate.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sudo kubectl create ns cert-manager
-sudo kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.7.0/cert-manager.yaml
-sudo kubectl apply -f cluster-issuer.yaml
-
+sudo kubectl create namespace entando
 
 dns=$HOSTNAME.$INSTRUQT_PARTICIPANT_ID.instruqt.io
 sed 's/CHANGE_THIS/'$dns'/' certificate-template.yaml > certificate.yaml
