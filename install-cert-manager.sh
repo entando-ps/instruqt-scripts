c86af5cbcefa505ccf0304fd489bed1b55ac67c4
GermanoGiudici <germano.giudici@gmail.com>
2022-02-11 19:30:34 +0100

    refactoring

--- install-cert-manager.sh
+++ install-cert-manager.sh
@@ -2,5 +2,6 @@
 echo "==============================START cert manager installation =============================="
 sudo kubectl create ns cert-manager
 sudo kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.7.0/cert-manager.yaml
+sleep 60
 sudo kubectl apply -f cluster-issuer.yaml
 echo "==============================END cert manager installation =============================="
