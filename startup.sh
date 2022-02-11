ac939817638ea4057e595c0bbbe7b727a4c9a957
GermanoGiudici <germano.giudici@gmail.com>
2022-02-11 21:07:25 +0100

    debugging

--- startup.sh
+++ startup.sh
@@ -18,7 +18,7 @@ chmod +x *.sh
 #source ./patch-kc.sh
 
 echo '****************START BASH PROFILE****************'
-cat <<EOT >> ../.bash_profile
+cat <<EOT >> ../.bashrc
 export NVM_DIR="\$HOME/.nvm"
 [ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "\$NVM_DIR/bash_completion" ] && \. "\$NVM_DIR/bash_completion"  # This loads nvm bash_completion
