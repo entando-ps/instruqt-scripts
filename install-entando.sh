788b3d88789bf8996d37eff7b4ae4810414a88bc
Sachin Baghel <sachin.baghel@newvisionsoftware.in>
2022-06-20 08:56:20 +0530

    Corrected the edit line numbers Corrected the operator apply command.

--- install-entando.sh
+++ install-entando.sh
@@ -11,15 +11,15 @@ sudo kubectl apply -n entando -f https://raw.githubusercontent.com/entando/entan
 curl -sLO "https://raw.githubusercontent.com/entando/entando-releases/v7.0.0/dist/ge-1-1-6/samples/entando-app.yaml"
 sed -i 's/  ingressHostName: YOUR-HOST-NAME/  ingressHostName: '${HOSTNAME}.${INSTRUQT_PARTICIPANT_ID}'.instruqt.io/g' entando-app.yaml
 sed -i 's/  environmentVariables: null/  environmentVariables:/g' entando-app.yaml
-sed -i '8i\    - name: "APPLICATIONBASEURL"' entando-app.yaml
-sed -i '9i\      value: "https://'${HOSTNAME}.${INSTRUQT_PARTICIPANT_ID}'.instruqt.io/entando-de-app/"' entando-app.yaml
+sed -i '9i\    - name: "APPLICATIONBASEURL"' entando-app.yaml
+sed -i '10i\      value: "https://'${HOSTNAME}.${INSTRUQT_PARTICIPANT_ID}'.instruqt.io/entando-de-app/"' entando-app.yaml
 
 chmod 777 entando-app.yaml
 
 curl -sLO "https://raw.githubusercontent.com/entando/entando-releases/v7.0.0/dist/ge-1-1-6/samples/entando-operator-config.yaml"
 sed -i "8i\  entando.tls.secret.name: entando-tls-secret" entando-operator-config.yaml
 
-sudo kubectl apply -f sample-configmaps/entando-operator-config.yaml -n entando
+sudo kubectl apply -f entando-operator-config.yaml -n entando
 sleep 1
 sudo kubectl apply -f entando-app.yaml -n entando
 
